ffebb1420e098cdf1856d27605f5b2d5dacbae51
Raghav Soni <soniraghav2207@gmail.com>
2024-04-08 13:54:55 +0530

    Day-1 learned how to use input, print and string concatenation.

---
+++ Day1_BandNameGenerator.py
@@ -0,0 +1,11 @@
+#Creating a greeting message
+print("Welcome to the Band Name Generator.")
+
+#Ask the user for the city they grew up in
+city_name = input("What's the name of the city you grew up in?\n")
+
+#Asking the user's pet's name
+pet_name = input("What's your pet's name?\n")
+
+#Combining the city_name and pet_name to show band name
+print("Your band name could be " + city_name + " " + pet_name)
