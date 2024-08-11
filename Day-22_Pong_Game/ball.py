c0313cf920d254f79d96bb15ad4e9a457e6222d2
Raghav Soni <soniraghav2207@gmail.com>
2024-08-11 12:05:57 +0530

    Day-22 learned to make a pong game and mastering turtle documentation

---
+++ Day-22_Pong_Game/ball.py
@@ -0,0 +1,30 @@
+from turtle import Turtle
+import time
+
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.shape("circle")
+        self.color("white")
+        self.x_increment = 10
+        self.y_increment = 10
+        self.move_speed = 0.1
+
+    def move(self):
+        new_x = self.xcor()+self.x_increment
+        new_y = self.ycor()+self.y_increment
+        self.goto(new_x, new_y)
+
+    def reset_position(self):
+        self.goto(0, 0)
+        self.bounce_x()
+        self.move_speed = 0.1
+
+    def bounce_y(self):
+        self.y_increment *= -1
+
+    def bounce_x(self):
+        self.x_increment *= -1
+        self.move_speed *= 0.9
