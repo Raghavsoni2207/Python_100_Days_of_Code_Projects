76e58e1727a63938fe763180de90efd63b23ce27
Raghav Soni <soniraghav2207@gmail.com>
2024-08-20 15:43:16 +0530

    Sortd FileNotFoundError and used absolute path

--- Day-24_Mail_Merge_Project/main.py
+++ Day-24_Mail_Merge_Project/main.py
@@ -1,12 +1,12 @@
 # Making a list of names
-with open("./Input/Names/invited_names.txt") as invited_names:
+with open("D:/Raghav soni/Git Files/Python_100_Days_of_Code_Projects/Day-24_Mail_Merge_Project/Input/Names/invited_names.txt") as invited_names:
     names = invited_names.readlines()
 
 for name in names:
     # Removing '\n' from names
     txt = name.strip()
-    with open(f"./Output/letter_for_{txt}.docx", "w") as file:
-        with open("./Input/Letters/starting_letter.txt") as starting_letter:
+    with open(f"D:/Raghav soni/Git Files/Python_100_Days_of_Code_Projects/Day-24_Mail_Merge_Project/Output/letter_for_{txt}.docx", "w") as file:
+        with open("D:/Raghav soni/Git Files/Python_100_Days_of_Code_Projects/Day-24_Mail_Merge_Project/Input/Letters/starting_letter.txt") as starting_letter:
             letter = starting_letter.read()
             # Replacing name and putting original name
             letter = letter.replace("[name]", txt)
