a732584c1224badf0aefcb3feb83db0e44f7afde
Raghav Soni <soniraghav2207@gmail.com>
2024-08-20 11:25:20 +0530

    Day-24 solve the bug in snake game created while updating high score bord and used file system to track the score board

--- Day-20_and_21_Snake_Game/scoreboard.py
+++ Day-20_and_21_Snake_Game/scoreboard.py
@@ -11,7 +11,9 @@ class Scoreboard(Turtle):
         super().__init__()
         self.score = 0
         # Track of high score
-        self.high_score = 0
+        # self.high_score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 265)
@@ -21,7 +23,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
-
+            with open("data.txt", "w") as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
 
